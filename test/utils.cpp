8390093454e03f935bf6bfe321629577be7f2039
4Marvin2 <mumeu222@mail.ru>
2021-03-08 18:35:42 +0300

    added tests

---
+++ test/utils.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "utils/utils.h"
+}
+
+TEST(utils, ok) {
+    char **ptr = new char*[4];
+    for(int i = 0; i < 4; i++) {
+        ptr[i] = new char[101];
+    }
+
+    ASSERT_EQ(pars_str("VTSS;VTSS;Atlantyda;1:30", ';', ptr), 4);
+
+    for(int i = 0; i < 4; i++) {
+        delete ptr[i];
+    }
+    delete []ptr;
+}
+
+TEST(utils, not_ok) {
+    char **ptr = new char*[4];
+    for(int i = 0; i < 4; i++) {
+        ptr[i] = new char[101];
+    }
+
+    ASSERT_EQ(pars_str("VTSS;VTSS;Atlantyda", ';', ptr), 3);
+
+    for(int i = 0; i < 4; i++) {
+        delete ptr[i];
+    }
+    delete []ptr;
+}
